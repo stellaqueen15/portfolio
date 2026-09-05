84c7fb309140ee7d99d5468c2ae5a8b605b2ed89
stellaqueen15 <brendamisenga@gmail.com>
2026-09-04 22:37:54 -0400

    add waves to the hero section

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,17 +1,136 @@
 export default function HeroSection() {
     return (
-        <section className="relative flex min-h-screen items-center justify-center px-6 pb-32">
+        <section className="relative flex min-h-screen items-center justify-center overflow-hidden px-6 pb-32">
             <div className="relative z-10 text-center">
                 <h1 className="text-6xl font-black leading-[0.7] tracking-tighter uppercase
                     sm:text-7xl md:text-8xl lg:text-[9rem]">
                     <span className="inline-block text-gradient">
                         Brenda
                     </span>
+
                     <br />
+
                     <span className="inline-block text-[6.5rem] text-gradient">
                         Développeuse web
                     </span>
                 </h1>
+            </div>
+
+            {/* Vague avant */}
+            <div className="absolute bottom-20 left-0 z-10 w-full overflow-hidden">
+                <svg
+                    className="block h-40 w-full"
+                    viewBox="0 0 1440 179"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    preserveAspectRatio="none"
+                >
+                    <g filter="url(#filter0_d_18_64)">
+                        <path
+                            d="M97 141.9C49.8581 139.243 1 68.4 1 68.4V2.40002H1439V96.4C1439 96.4 1370.07 142.112 1320.5 141.9C1271.48 141.69 1252.44 93.4736 1203.5 96.4C1150.75 99.5547 1130 164.4 1086.5 164.4C1043 164.4 1018.8 101.563 964.5 96.4C909.299 91.1519 884.466 152.297 830 141.9C787.496 133.787 780.556 85.21 737.5 80.9C695.107 76.6564 678.414 116.866 636 120.9C572.93 126.899 545.442 61.1841 482.5 68.4C424.896 75.0039 410.838 133.286 353.5 141.9C288.33 151.691 255.862 84.745 191 96.4C150.859 103.613 137.719 144.195 97 141.9Z"
+                            fill="#FFA240"
+                        />
+                    </g>
+
+                    <defs>
+                        <filter
+                            id="filter0_d_18_64"
+                            x="-7.4"
+                            y="2.47955e-05"
+                            width="1454.8"
+                            height="178.8"
+                            filterUnits="userSpaceOnUse"
+                            colorInterpolationFilters="sRGB"
+                        >
+                            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                            <feColorMatrix
+                                in="SourceAlpha"
+                                type="matrix"
+                                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                                result="hardAlpha"
+                            />
+                            <feOffset dy="6" />
+                            <feGaussianBlur stdDeviation="4.2" />
+                            <feComposite in2="hardAlpha" operator="out" />
+                            <feColorMatrix
+                                type="matrix"
+                                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.07 0"
+                            />
+                            <feBlend
+                                mode="normal"
+                                in2="BackgroundImageFix"
+                                result="effect1_dropShadow_18_64"
+                            />
+                            <feBlend
+                                mode="normal"
+                                in="SourceGraphic"
+                                in2="effect1_dropShadow_18_64"
+                                result="shape"
+                            />
+                        </filter>
+                    </defs>
+                </svg>
+            </div>
+
+            <div className="absolute bottom-6 left-0 z-0 w-full overflow-hidden">
+                <svg
+                    className="block h-40 w-full"
+                    viewBox="0 0 1440 234"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    preserveAspectRatio="none"
+                >
+                    <g filter="url(#filter0_d_19_65)">
+                        <path
+                            d="M62 163.4C34.9974 167.858 1 128.9 1 128.9V2.40002L1439 20.4V138.4C1439 138.4 1424.91 166.413 1408 172.9C1380.36 183.503 1369.48 132.347 1340.5 138.4C1313.86 143.965 1318.29 179.341 1293 189.4C1253.56 205.085 1228.18 131.438 1191 151.9C1169.12 163.939 1175.59 191.03 1154.5 204.4C1107.44 234.235 1080.17 118.83 1028 138.4C1000.84 148.587 1001.72 180.842 974 189.4C925.411 204.403 916.253 114.22 865.5 117.4C834.206 119.361 823.972 147.015 793 151.9C740.532 160.176 716.818 94.2239 665 105.9C623.617 115.225 621.75 169.1 579.5 172.9C539.834 176.468 526.825 122.605 487.5 128.9C442.642 136.08 456.043 206.261 412 217.4C356.495 231.438 345.504 133.062 288.5 138.4C249.289 142.072 240.881 189.801 201.5 189.4C155.417 188.931 151.84 103.19 108 117.4C83.8328 125.234 87.0658 159.262 62 163.4Z"
+                            fill="#FF9739"
+                        />
+                    </g>
+
+                    <defs>
+                        <filter
+                            id="filter0_d_19_65"
+                            x="-7.4"
+                            y="2.47955e-05"
+                            width="1454.8"
+                            height="233.173"
+                            filterUnits="userSpaceOnUse"
+                            colorInterpolationFilters="sRGB"
+                        >
+                            <feFlood
+                                floodOpacity="0"
+                                result="BackgroundImageFix"
+                            />
+                            <feColorMatrix
+                                in="SourceAlpha"
+                                type="matrix"
+                                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                                result="hardAlpha"
+                            />
+                            <feOffset dy="6" />
+                            <feGaussianBlur stdDeviation="4.2" />
+                            <feComposite
+                                in2="hardAlpha"
+                                operator="out"
+                            />
+                            <feColorMatrix
+                                type="matrix"
+                                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.07 0"
+                            />
+                            <feBlend
+                                mode="normal"
+                                in2="BackgroundImageFix"
+                                result="effect1_dropShadow_19_65"
+                            />
+                            <feBlend
+                                mode="normal"
+                                in="SourceGraphic"
+                                in2="effect1_dropShadow_19_65"
+                                result="shape"
+                            />
+                        </filter>
+                    </defs>
+                </svg>
             </div>
         </section>
     )
