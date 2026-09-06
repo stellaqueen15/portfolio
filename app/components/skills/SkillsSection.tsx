e1479e0e4d00383852c0c4f4aaff81dc43aa1e1e
stellaqueen15 <brendamisenga@gmail.com>
2026-09-06 11:24:41 -0400

    change title

--- app/components/skills/SkillsSection.tsx
+++ app/components/skills/SkillsSection.tsx
@@ -7,9 +7,9 @@ export default function SkillsSection() {
             className="relative flex min-h-screen items-center overflow-hidden bg-background-reverse px-6 py-24"
         >
             <div className="mx-auto w-full max-w-350">
-                <div className="mb-10 text-center">
-                    <h2 className="text-5xl font-black uppercase md:text-7xl">
-                        Skills
+                <div className="text-center">
+                    <h2 className="text-5xl text-gradient uppercase font-black tracking-tight md:text-7xl">
+                        Ma petite boîte à outil
                     </h2>
                 </div>
 
